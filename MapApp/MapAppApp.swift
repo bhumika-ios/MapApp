61ce178facca1f653aa19a5c087e9fd78d74247a
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-09-02 13:47:14 +0530

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Bhumika Patel on 02/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
